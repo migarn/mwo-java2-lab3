92fbe62772ac2d43feb66bfbfc3aa7cdf151882c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-19 18:45:00 +0100

    Strucutre of mainMenu method introduced

--- src/zadanie2/Commander.java
+++ src/zadanie2/Commander.java
@@ -1,17 +1,39 @@
 package zadanie2;
 
 public class Commander {
+	private static UserScanner scanner = new UserScanner();
 	
 	public static void main(String args[]) {
 		mainMenu();
 	}
 	
 	public static void mainMenu() {
-		System.out.println("\nType: \n1 - to print simple files view"
-				+ "\n2 - to print detailed files view"
-				+ "\n3 - to print files with specific extension"
-				+ "\n4 - to print files tree"
-				+ "\n5 - to exit");
-	}
+		boolean inLoop = true;
+		String instruction = "\nType: \n1 - to print simple files view" + 
+				"\n2 - to print detailed files view" + 
+				"\n3 - to print files with specific extension" + 
+				"\n4 - to print files tree" + 
+				"\n5 - to exit";
 		
+		while (inLoop) {
+			int choice = scanner.scanSelectionList(instruction, 1, 2, 3, 4, 5);
+			
+			if (choice == 1) {
+				//simple files view
+			}
+			else if (choice == 2) {
+				//detailed files view
+			}
+			else if (choice == 3) {
+				// files view with extension
+			}
+			else if (choice == 4) {
+				//files tree
+			}
+			else {
+				System.out.println("\nApplication terminated.");
+				inLoop = false;
+			}
+		}
+	}
 }
