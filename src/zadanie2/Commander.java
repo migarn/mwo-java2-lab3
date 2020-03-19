2f81ba61059015a1826e3b8f7e460afb60eff3b7
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-19 18:30:40 +0100

    mainMenu method added

--- src/zadanie2/Commander.java
+++ src/zadanie2/Commander.java
@@ -3,7 +3,15 @@ package zadanie2;
 public class Commander {
 	
 	public static void main(String args[]) {
-		//TODO to be implemented
+		mainMenu();
+	}
+	
+	public static void mainMenu() {
+		System.out.println("\nType: \n1 - to print simple files view"
+				+ "\n2 - to print detailed files view"
+				+ "\n3 - to print files with specific extension"
+				+ "\n4 - to print files tree"
+				+ "\n5 - to exit");
 	}
 		
 }
