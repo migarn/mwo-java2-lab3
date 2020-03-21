dab690e4b2ce9ae3f74c83cd7f9f200d39c0c076
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-21 11:47:04 +0100

    FileOutputStream works

--- src/cwiczenie5/Demo.java
+++ src/cwiczenie5/Demo.java
@@ -1,9 +1,38 @@
 package cwiczenie5;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  try {
+		         FileOutputStream fileOut =
+		         new FileOutputStream("res/cwiczenie5/dane.ser");
+		         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		         out.writeObject(String.valueOf("jakas nazwa"));
+		         out.writeObject(Integer.valueOf(5));
+		         out.close();
+		         fileOut.close();
+		    } catch(IOException i) {
+		         i.printStackTrace();
+		    }
+		
+		
+		
+		
+//		try {
+//			FileInputStream fileIn = new FileInputStream("res/cwiczenie5/dane.ser");
+//			ObjectInputStream in = new ObjectInputStream(fileIn);
+////			in.writeObject(x);
+//			in.close();
+//			fileIn.close();
+//		} catch (IOException i) {
+//			i.printStackTrace();
+//		}
 
 	}
 
