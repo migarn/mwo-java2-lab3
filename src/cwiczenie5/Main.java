6155e0ddd25d45ede92aee6ec3b1b6cf1e7dcb5e
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-21 13:16:03 +0100

    Another agent added

--- src/cwiczenie5/Main.java
+++ src/cwiczenie5/Main.java
@@ -55,16 +55,25 @@ public class Main {
 
 		Task task = new Task("darkMamba", "infiltrate, spy, destroy");
 		agent007.addTask(task);
+		
+		Agent agentX = new Agent("Jerzy", "Malina", 85, "123");
+		bureau.addAgent(agentX);
+		
+		Task task2 = new Task("Secret Mission", "To be secret");
+		agentX.addTask(task2);
+		
+		Task task3 = new Task("More Secret Mission", "To be more secret");
+		agentX.addTask(task3);
 
 		return bureau;
 	}
 	
 	private static void printBureau(Bureau bureau) {
-		System.out.println("\nBureau name: " + bureau.getName() + "\n");
+		System.out.println("\nBureau name: " + bureau.getName());
 		List<Agent> agents = bureau.getAgents();
 		
 		for (Agent agent : agents) {
-			System.out.println("Agent " + agent.getName() + " " + agent.getSurname() + " " + agent.getNumber());
+			System.out.println("\nAgent " + agent.getName() + " " + agent.getSurname() + " " + agent.getNumber());
 			List<Task> tasks = agent.getTasks();
 			System.out.println("Tasks:");
 			for (Task task : tasks) {
